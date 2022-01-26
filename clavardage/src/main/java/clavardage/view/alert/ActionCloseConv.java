83368e36bdd3ddfdae1d616ea655b87ff9e52ed1
clestinepa <paille@etud.insa-toulouse.fr>
2022-01-26 18:56:57 +0100

    close conv Group ok

--- clavardage/src/main/java/clavardage/view/alert/ActionCloseConv.java
+++ clavardage/src/main/java/clavardage/view/alert/ActionCloseConv.java
@@ -2,10 +2,10 @@ package clavardage.view.alert;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.UUID;
 
 import clavardage.controller.gui.MainGUI;
 import clavardage.view.Application;
+import clavardage.view.Application.Destinataire;
 import clavardage.view.listener.MouseCloseConversation;
 import clavardage.view.mystyle.MyJButtonText;
 
@@ -17,7 +17,11 @@ public class ActionCloseConv implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		try {
-			MainGUI.closeConversation(Application.getMessageWindow().getDiscussionDisplay().getIdConversation());
+			if (Application.getMessageWindow().getDiscussionDisplay().getTypeConversation() == Destinataire.User) {
+				MainGUI.closeConversation(Application.getMessageWindow().getDiscussionDisplay().getIdConversation());
+			} else {
+				MouseCloseConversation.closeConversation(Application.getMessageWindow().getDiscussionDisplay().getIdConversation());
+			}
 			((MyJButtonText) e.getSource()).setForeground(Application.getBLUE());
 			Application.getMessageWindow().setAlertOpen(false) ;
 		} catch (Exception ex) {
